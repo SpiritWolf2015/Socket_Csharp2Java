2cbe20ab60728fc59adc32b73465ac8aefd5a24c
SpiritWolf2015 <522414019@qq.com>
2015-05-19 17:40:27 +0800

    1.2
    C#客户端发送登录数据给JAVA服务器已经OK，接受JAVA返回的登录是否成功的字符串还有问题。

---
+++ TcpClient/CsharpTcpClient_TcpClient/MessageType.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CsharpTcpClient_TcpClient {
+    static class MessageType {
+        public static readonly string message_succeed = "1";// 表明是登陆成功
+        public static readonly string message_login_fail = "2";// 表明登录失败
+        public static readonly string message_comm_mes = "3";// 普通信息包
+        public static readonly string message_get_onLineFriend = "4";// 要求在线好友的包
+        public static readonly string message_ret_onLineFriend = "5";// 返回在线好友的包
+    }
+}
--- TcpClient/CsharpTcpClient_TcpClient/QqClientConServer.cs
+++ TcpClient/CsharpTcpClient_TcpClient/QqClientConServer.cs
@@ -20,19 +20,42 @@ namespace CsharpTcpClient_TcpClient {
                 m_client.Connect(new System.Net.IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999));
 
                 using (NetworkStream networkStream = m_client.GetStream( )) {
+                    #region 往JAVA服务器发送ID，密码
+
                     using (BinaryWriter binaryWriter = new BinaryWriter(networkStream, Encoding.UTF8)) {
                         // 用户数据，用 | 来拼接分隔
-                        string strSendData = u.UserId + "|" + u.Passwd + "\r\n";
+                        string strSendData = u.UserId + "|" + u.Passwd +"\r\n";
                         // 将字符串写到流中，发出
                         binaryWriter.Write(strSendData);
                         binaryWriter.Flush( );
-                        Console.WriteLine(string.Format("用户登录ID = {0} Passwd = {1}", u.UserId, u.Passwd));
+                        Console.WriteLine(string.Format("用户登录ID = {0} Passwd = {1}", u.UserId, u.Passwd));                       
+
+                        #region 接受JAVA发来的数据判断，登录是否成功
+                        using (MemoryStream memStream = new MemoryStream( )) {
+                            
+                            //接收数据
+                            byte[ ] buffer = new byte[1024];
+                            int recCount = 0;
+                            // 接收返回的字节流
+                            while ((recCount = networkStream.Read(buffer, 0, 1024)) > 0) {
+                                Console.WriteLine("接收数据");
+                                memStream.Write(buffer, 0, recCount);
+                                Console.WriteLine(string.Format("recCount = {0}", recCount));
+                            }
+                            Encoding encoding = Encoding.GetEncoding("GBK");
+                            string returnMsg = encoding.GetString(memStream.GetBuffer( ), 0, memStream.GetBuffer( ).Length);
+                            Console.WriteLine(string.Format("接受JAVA返回的字符串 = {0}", returnMsg));
+
+                            memStream.Close( );
+                        }
+                        #endregion 接受JAVA发来的数据判断，登录是否成功
 
                         binaryWriter.Close( );
-                        networkStream.Close( );                        
                     }
-                }
+                    #endregion 往JAVA服务器发送ID，密码
 
+                    networkStream.Close( );  
+                }
             } catch (Exception e) {
                 ExceptionHandle(e);
             } finally {
@@ -50,6 +73,7 @@ namespace CsharpTcpClient_TcpClient {
         }
 
         void ExceptionHandle (Exception e) {
+            Console.WriteLine(e.StackTrace);
             Console.WriteLine(e.Message);
             Console.WriteLine(e.Source);
         }
