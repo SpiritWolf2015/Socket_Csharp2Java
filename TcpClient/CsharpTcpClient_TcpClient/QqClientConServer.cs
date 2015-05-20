68788159efe2353f0ff86ef4a0aba682b8e3a31d
SpiritWolf2015 <522414019@qq.com>
2015-05-20 17:47:21 +0800

    1.3
    JAVA与C#的TCP通信，登录模块的模拟已经OK。

--- TcpClient/CsharpTcpClient_TcpClient/QqClientConServer.cs
+++ TcpClient/CsharpTcpClient_TcpClient/QqClientConServer.cs
@@ -14,44 +14,40 @@ namespace CsharpTcpClient_TcpClient {
         public bool sendLoginInfoToServer (User u) {
             bool isLoginOk = false;
             try {
-                Console.WriteLine("C#客户端启动，去连接PORT = 9999 本机JAVA服务器");
+                Console.WriteLine("C#客户端启动，去连接PORT = 9999 的本机JAVA服务器");
                 m_client = new TcpClient( );
                 // 去连JAVA服务器
                 m_client.Connect(new System.Net.IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999));
 
                 using (NetworkStream networkStream = m_client.GetStream( )) {
                     #region 往JAVA服务器发送ID，密码
+                    // 用户数据，用 | 来拼接分隔，结尾用\r\n标识一行文本终止。
+                    string strSendData = u.UserId + "|" + u.Passwd + "\r\n";
+                    
+                    byte[ ] data = System.Text.Encoding.UTF8.GetBytes(strSendData);
+                    Console.WriteLine(string.Format("data.Length = {0} ", data.Length));
+                    // 将字符串写到流中，发出
+                    networkStream.Write(data, 0, data.Length);
+                    networkStream.Flush( );
+                    Console.WriteLine(string.Format("用户登录ID = {0} Passwd = {1}", u.UserId, u.Passwd));
 
-                    using (BinaryWriter binaryWriter = new BinaryWriter(networkStream, Encoding.UTF8)) {
-                        // 用户数据，用 | 来拼接分隔
-                        string strSendData = u.UserId + "|" + u.Passwd +"\r\n";
-                        // 将字符串写到流中，发出
-                        binaryWriter.Write(strSendData);
-                        binaryWriter.Flush( );
-                        Console.WriteLine(string.Format("用户登录ID = {0} Passwd = {1}", u.UserId, u.Passwd));                       
 
-                        #region 接受JAVA发来的数据判断，登录是否成功
-                        using (MemoryStream memStream = new MemoryStream( )) {
-                            
-                            //接收数据
-                            byte[ ] buffer = new byte[1024];
-                            int recCount = 0;
-                            // 接收返回的字节流
-                            while ((recCount = networkStream.Read(buffer, 0, 1024)) > 0) {
-                                Console.WriteLine("接收数据");
-                                memStream.Write(buffer, 0, recCount);
-                                Console.WriteLine(string.Format("recCount = {0}", recCount));
-                            }
-                            Encoding encoding = Encoding.GetEncoding("GBK");
-                            string returnMsg = encoding.GetString(memStream.GetBuffer( ), 0, memStream.GetBuffer( ).Length);
-                            Console.WriteLine(string.Format("接受JAVA返回的字符串 = {0}", returnMsg));
+                    #region 接受JAVA发来的数据判断，登录是否成功     
 
-                            memStream.Close( );
-                        }
-                        #endregion 接受JAVA发来的数据判断，登录是否成功
+                    //接收数据
+                    byte[ ] buffer = new byte[4];                  
+                    // 读取返回的字节流
+                     int recCount = networkStream.Read(buffer, 0, buffer.Length);
+                    string returnMsg = System.Text.Encoding.UTF8.GetString(buffer, 0, recCount);
+                    Console.WriteLine(string.Format("接受JAVA返回的字符串 = {0}", returnMsg));
 
-                        binaryWriter.Close( );
-                    }
+                    if (MessageType.message_succeed.Equals(returnMsg)) {
+                        // 登录成功
+                        isLoginOk = true;
+                    }               
+                    #endregion 接受JAVA发来的数据判断，登录是否成功
+
+                 
                     #endregion 往JAVA服务器发送ID，密码
 
                     networkStream.Close( );  
